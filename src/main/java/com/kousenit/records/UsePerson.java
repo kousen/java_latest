7cbe9eb0fee203fce4e43cf7410c6f54f0c49681
Ken Kousen <ken.kousen@kousenit.com>
2020-09-11 10:02:11 -0400

    Add records to set

--- src/main/java/com/kousenit/records/UsePerson.java
+++ src/main/java/com/kousenit/records/UsePerson.java
@@ -1,5 +1,6 @@
 package com.kousenit.records;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class UsePerson {
@@ -8,8 +9,9 @@ public class UsePerson {
         Person p2 = new Person(1, "Jean-Luc", "Picard");
         System.out.println(p1);
         System.out.println(p1.equals(p2));
-//        Set<Person> people = Set.of(p1, p2);
-//        System.out.println(people);
+        Set<Person> people = new HashSet<>();
+        people.add(p1); people.add(p2);  // only adds unique elements
+        System.out.println(people);
         System.out.println(p1.first() + " " + p1.last());
     }
 }
