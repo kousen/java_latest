170f4d9c43d00889c9779fee5576d817fa299eb8
Ken Kousen <ken.kousen@kousenit.com>
2021-01-13 10:57:02 -0500

    Add more examples

--- src/test/java/com/kousenit/http/JokeClientTest.java
+++ src/test/java/com/kousenit/http/JokeClientTest.java
@@ -16,8 +16,8 @@ class JokeClientTest {
     private final Logger logger = Logger.getLogger(JokeClientTest.class.getName());
 
     private final JokeClient client = new JokeClient();
-    private final String heroFirstName = "Nurul";
-    private final String heroLastName = "Ishak";
+    private final String heroFirstName = "Andrew";
+    private final String heroLastName = "Ehrig";
 
     @BeforeEach
     void setUp() throws IOException {
