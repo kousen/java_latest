913404d1d350a32913c30da4fa3b6167b60f57dc
Ken Kousen <ken.kousen@kousenit.com>
2021-01-23 17:31:32 -0500

    Add pattern matching example with Point

---
+++ src/main/java/com/kousenit/patternmatching/Point.java
@@ -0,0 +1,49 @@
+package com.kousenit.patternmatching;
+
+import java.util.Objects;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    // Pattern matching
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        return (o instanceof Point p)
+                && x == p.x && y == p.y;
+    }
+
+//    Normal, generated
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Point point = (Point) o;
+//        return x == point.x && y == point.y;
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
---
+++ src/test/java/com/kousenit/patternmatching/PointTest.java
@@ -0,0 +1,26 @@
+package com.kousenit.patternmatching;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    @Test
+    void patternMatchingEquals() {
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(3, 4);  // p1, p2 are equal but different
+        Point p3 = p1;               // p1, p3 both point to same instance
+        Point p4 = new Point(5, 6);  // p4 different from p1, p2, p3
+        Point p5 = null;             // p5 is null
+
+        assertAll(
+                () -> assertEquals(p1, p2, "equal but different objects"),
+                () -> assertEquals(p1, p3, "two references to same object are equal"),
+                () -> assertSame(p1, p3, "two references to same object"),
+                () -> assertNotSame(p2, p3, "equal but different points"),
+                () -> assertNotEquals(p1, p4, "not equal"),
+                () -> assertNotEquals(p1, p5)
+        );
+    }
+}
