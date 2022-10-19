7e82f62dd81b13309ee0b9a7aa0656c3aacde3fe
Ken Kousen <ken.kousen@kousenit.com>
2022-10-19 07:52:32 -0700

    Upgrade dependencies and fix assumptions in Http client test

--- src/test/java/com/kousenit/http/JokeClientTest.java
+++ src/test/java/com/kousenit/http/JokeClientTest.java
@@ -1,14 +1,18 @@
 package com.kousenit.http;
 
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class JokeClientTest {
     private final Logger logger = Logger.getLogger(JokeClientTest.class.getName());
@@ -18,10 +22,18 @@ class JokeClientTest {
     private final String heroLastName = "Subramaniam";
 
     @BeforeEach
-    void setUp() throws IOException {
-        Assumptions.assumeTrue(
-                InetAddress.getByName("icndb.com").isReachable(2000),
+    void setUp() throws IOException, InterruptedException {
+        assumeTrue(
+                InetAddress.getByName("icndb.com")
+                        .isReachable(2000),
                 "ICNDB API site is down");
+        HttpResponse<Void> response = HttpClient.newHttpClient()
+                .send(HttpRequest.newBuilder()
+                                .uri(URI.create("http://icndb.com"))
+                                .HEAD()
+                                .build(),
+                        HttpResponse.BodyHandlers.discarding());
+        assumeTrue(response.statusCode() == 200, "ICNDB API site is down");
     }
 
     @Test
@@ -29,7 +41,7 @@ class JokeClientTest {
         String joke = client.getJokeSync(heroFirstName, heroLastName);
         logger.info(joke);
         assertTrue(joke.contains(heroFirstName) ||
-                           joke.contains(heroLastName));
+                joke.contains(heroLastName));
     }
 
     @Test
@@ -37,6 +49,6 @@ class JokeClientTest {
         String joke = client.getJokeAsync(heroFirstName, heroLastName);
         logger.info(joke);
         assertTrue(joke.contains(heroFirstName) ||
-                           joke.contains(heroLastName));
+                joke.contains(heroLastName));
     }
 }
