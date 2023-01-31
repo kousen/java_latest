7bb4559dc6b84594a5463e9c3ce7b6175140fdc5
Ken Kousen <ken.kousen@kousenit.com>
2023-01-31 18:25:22 -0500

    Add Period tests to datetime calculation

--- src/main/java/com/kousenit/datetime/DateUtils.java
+++ src/main/java/com/kousenit/datetime/DateUtils.java
@@ -1,6 +1,7 @@
 package com.kousenit.datetime;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 public class DateUtils {
@@ -11,5 +12,9 @@ public class DateUtils {
 
     public static long weeksBetween(LocalDate date1, LocalDate date2) {
         return ChronoUnit.WEEKS.between(date1, date2);
+    }
+
+    public static Period periodBetween(LocalDate date1, LocalDate date2) {
+        return Period.between(date1, date2);
     }
 }
--- src/test/java/com/kousenit/datetime/DateUtilsTest.java
+++ src/test/java/com/kousenit/datetime/DateUtilsTest.java
@@ -1,57 +1,77 @@
 package com.kousenit.datetime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DateUtilsTest {
 
     @Test
     void daysBetweenSameDay() {
         long daysBetween = DateUtils.daysBetween(
-                LocalDate.of(2023, 2, 2),
-                LocalDate.of(2023, 2, 2));
+                LocalDate.of(2023, Month.FEBRUARY, 2),
+                LocalDate.of(2023, Month.FEBRUARY, 2));
         assertEquals(0, daysBetween);
     }
 
     @Test
     void daysBetweenSameMonth() {
         long daysBetween = DateUtils.daysBetween(
-                LocalDate.of(2023, 2, 2),
-                LocalDate.of(2023, 2, 3));
+                LocalDate.of(2023, Month.FEBRUARY, 2),
+                LocalDate.of(2023, Month.FEBRUARY, 3));
         assertEquals(1, daysBetween);
     }
 
     @Test
     void daysBetweenSameYear() {
         long daysBetween = DateUtils.daysBetween(
-                LocalDate.of(2023, 2, 2),
-                LocalDate.of(2023, 3, 2));
+                LocalDate.of(2023, Month.FEBRUARY, 2),
+                LocalDate.of(2023, Month.MARCH, 2));
         assertEquals(28, daysBetween);
     }
 
     @Test
     void daysBetweenDifferentYears() {
         long daysBetween = DateUtils.daysBetween(
-                LocalDate.of(2022, 2, 2),
-                LocalDate.of(2023, 2, 2));
+                LocalDate.of(2022, Month.FEBRUARY, 2),
+                LocalDate.of(2023, Month.FEBRUARY, 2));
         assertEquals(365, daysBetween);
     }
 
     @Test
     void daysBetweenDifferentYearsLeapYear() {
         long daysBetween = DateUtils.daysBetween(
-                LocalDate.of(2020, 2, 2),
-                LocalDate.of(2021, 2, 2));
+                LocalDate.of(2020, Month.FEBRUARY, 2),
+                LocalDate.of(2021, Month.FEBRUARY, 2));
         assertEquals(366, daysBetween);
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
+    void periodBetween() {
+        Period period = DateUtils.periodBetween(
+                LocalDate.of(2023, Month.FEBRUARY, 2),
+                LocalDate.of(2023, Month.MARCH, 20));
+        assertAll(
+                () -> assertEquals(0, period.getYears()),
+                () -> assertEquals(1, period.getMonths()),
+                () -> assertEquals(18, period.getDays()),  // Careful! 18 days, not 46
+                () -> assertThrows(UnsupportedTemporalTypeException.class,
+                        () -> period.get(ChronoUnit.WEEKS))  // No getWeeks() method
+        );
+    }
+
+    @Test @DisplayName("Groundhog Day to first day of spring")
     void groundHogDayToFirstDayOfSpring() {
-        LocalDate groundHogDay = LocalDate.of(2023, 2, 2);
-        LocalDate firstDayOfSpring = LocalDate.of(2023, 3, 20);
+        LocalDate groundHogDay = LocalDate.of(2023, Month.FEBRUARY, 2);
+        LocalDate firstDayOfSpring = LocalDate.of(2023, Month.MARCH, 20);
         long daysBetween = DateUtils.daysBetween(groundHogDay, firstDayOfSpring);
         assertEquals(46, daysBetween);
 
@@ -62,4 +82,5 @@ class DateUtilsTest {
         System.out.println("There are " + weeks + " weeks and " + days +
                 " days between Groundhog Day and the first day of spring");
     }
+
 }
