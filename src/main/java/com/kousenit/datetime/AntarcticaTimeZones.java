89aa32d528bfcfe128ba7068cabb019e6bd04cc9
Ken Kousen <ken.kousen@kousenit.com>
2023-03-01 18:01:27 -0500

    Update dependencies

--- src/main/java/com/kousenit/datetime/AntarcticaTimeZones.java
+++ src/main/java/com/kousenit/datetime/AntarcticaTimeZones.java
@@ -22,7 +22,7 @@ public class AntarcticaTimeZones {
                         .toList();
 
         antarcticZones.forEach(zdt ->
-                System.out.printf("%7s: %25s %7s%n", zdt.getOffset(), zdt.getZone(),
+                System.out.printf("UTC%6s: %25s %7s%n", zdt.getOffset(), zdt.getZone(),
                         zdt.getZone().getRules().isDaylightSavings(zdt.toInstant())));
     }
 }
