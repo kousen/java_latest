9e6537e504c61f7d2e3de024c9f0d386921cb9f0
Ken Kousen <ken.kousen@kousenit.com>
2023-10-25 23:40:07 -0400

    Remove commented out eclipse settings in build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,22 +47,3 @@ tasks.test {
     useJUnitPlatform()
     maxParallelForks = Runtime.getRuntime().availableProcessors() / 2 + 1
 }
-
-// See: https://github.com/redhat-developer/vscode-java/wiki/Enabling-Java-preview-features
-//Buildship doesn't use that hook (https://discuss.gradle.org/t/when-does-buildship-eclipse-customization-run/20781/2)
-//you need to run `gradlew eclipse` separately
-//eclipse.jdt.file.withProperties { props: Properties ->
-//    props["org.eclipse.jdt.core.compiler.problem.enablePreviewFeatures"] = "enabled"
-//    props["org.eclipse.jdt.core.compiler.problem.reportPreviewFeatures"] = "ignore"
-//}
-
-//eclipse {
-//    jdt {
-//        file {
-//            withProperties {
-//                set("org.eclipse.jdt.core.compiler.problem.enablePreviewFeatures", "enabled")
-//                set("org.eclipse.jdt.core.compiler.problem.reportPreviewFeatures", "ignore")
-//            }
-//        }
-//    }
-//}
